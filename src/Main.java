63e070f02e8ecbcd27076d1a560079eafe68b536
Kuba S <144416165+KubicaDzejkop@users.noreply.github.com>
2025-07-25 14:07:22 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,21 @@ public class Main {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Program liczy pole pod wykresem funkcji");
+        System.out.println("The program calculates the area under the graph of a function");
 
         double[] coefficients = getCoefficients();
         printFunction(coefficients);
 
-        System.out.println("Podaj początek i koniec przedziału");
+        System.out.println("Specify the beginning and end of the interval");
         double startInterval = scanner.nextDouble();
         double endInterval = scanner.nextDouble();
-        System.out.println("Podaj liczbę przedziałów (im większa to dokładność jest większa, lecz czas" +
-                " się przedłuża)");
+        System.out.println("Enter the number of intervals (the larger the interval, the greater the accuracy, but the time is longer");
         int numberIntervals = scanner.nextInt();
         double area = calculateFunctionArea(coefficients,startInterval, endInterval, numberIntervals);
-        System.out.printf("Pole pod wykresem funkcji wynosi %.3f\n ", area);
+        System.out.printf("The area under the graph of the function is %.3f\n ", area);
     }
 
-    public static double calculateFunctionArea(double[] coefficients, double startInterval, double endInterval, int numberIntervals) { // start przedzialu
+    public static double calculateFunctionArea(double[] coefficients, double startInterval, double endInterval, int numberIntervals) { 
         double area = 0;
         double interval = (endInterval - startInterval) / numberIntervals;
         // f(x) = x^2 - 3x + 2
@@ -40,11 +39,11 @@ public class Main {
     }
 
     public static double[] getCoefficients() {
-        System.out.println("Podaj stopień wielomianu");
+        System.out.println("Enter the degree of the polynomial");
         int degree = scanner.nextInt(); // stopien
 
         double[] coefficients = new double[degree + 1];
-        System.out.println("Podaj współczynniki wielomianu w ilości " + (degree + 1));
+        System.out.println("Enter the coefficients of the polynomial in quantity " + (degree + 1));
         for(int i = 0 ; i <= degree; i++){
             coefficients[i] = scanner.nextDouble();
         }
